5ae98c0e89184f7432eebe869efbaf88a9c77da0
Luyen Tran <tranminhluyen.1993@gmail.com>
2022-05-31 17:19:45 +0700

    update

--- pages/builder.vue
+++ pages/builder.vue
@@ -1,8 +1,34 @@
 <template>
   <div class="w-full h-full flex">
-    <div class="w-1/5 border-r border-black"></div>
+    <div class="w-1/5 border-r border-black p-3 flex flex-col space-y-1">
+      <button class="bg-gray-800 px-2 text-gray-400 w-full">EDIT</button>
+      <button class="bg-gray-800 px-2 text-gray-400 w-full">EDIT</button>
+      <button class="bg-gray-800 px-2 text-gray-400 w-full">EDIT</button>
+      <button class="bg-gray-800 px-2 text-gray-400 w-full">EDIT</button>
+      <button class="bg-gray-800 px-2 text-gray-400 w-full">EDIT</button>
+      <button class="bg-gray-800 px-2 text-gray-400 w-full">EDIT</button>
+      <button class="bg-gray-800 px-2 text-gray-400 w-full">EDIT</button>
+      <button class="bg-gray-800 px-2 text-gray-400 w-full">EDIT</button>
+
+    </div>
     <div class="w-3/5"></div>
-    <div class="w-1/5 border-l border-black"></div>
+    <div class="w-1/5 border-l border-black  p-3 flex flex-col space-y-1">
+      <button class="bg-gray-800 px-2 text-gray-400 w-full">ADD</button>
+      <button class="bg-gray-800 px-2 text-gray-400 w-full">UPDATE</button>
+      <button class="bg-gray-800 px-2 text-gray-400 w-full">EDIT</button>
+      <button class="bg-gray-800 px-2 text-gray-400 w-full">ADD</button>
+      <button class="bg-gray-800 px-2 text-gray-400 w-full">UPDATE</button>
+      <button class="bg-gray-800 px-2 text-gray-400 w-full">EDIT</button>
+      <button class="bg-gray-800 px-2 text-gray-400 w-full">ADD</button>
+      <button class="bg-gray-800 px-2 text-gray-400 w-full">UPDATE</button>
+      <button class="bg-gray-800 px-2 text-gray-400 w-full">EDIT</button>
+      <button class="bg-gray-800 px-2 text-gray-400 w-full">ADD</button>
+      <button class="bg-gray-800 px-2 text-gray-400 w-full">UPDATE</button>
+      <button class="bg-gray-800 px-2 text-gray-400 w-full">EDIT</button>
+      <button class="bg-gray-800 px-2 text-gray-400 w-full">ADD</button>
+      <button class="bg-gray-800 px-2 text-gray-400 w-full">UPDATE</button>
+      <button class="bg-gray-800 px-2 text-gray-400 w-full">EDIT</button>
+    </div>
   </div>
 </template>
 
