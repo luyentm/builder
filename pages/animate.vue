ba21bc5304936416083b4648cb8d503db9fb73c3
Luyen Tran <tranminhluyen.1993@gmail.com>
2022-05-31 16:56:14 +0700

    udpate

---
+++ pages/animate.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="w-full h-full flex items-center justify-center relative">
+    <div class="absolute w-70 h-70 flex justify-center items-center bg-yellow-200  animate-duration-1000 animate-delay-100 animate-ping animate-loop">
+    </div>
+    <div class="absolute w-60 h-60 flex justify-center items-center bg-yellow-200  animate-duration-1000 animate-delay-200 animate-ping animate-loop">
+    </div>
+    <div class="absolute w-50 h-50 flex justify-center items-center bg-yellow-200  animate-duration-1000 animate-delay-300 animate-ping animate-loop">
+    </div>
+    <div class="absolute w-40 h-40 flex justify-center items-center bg-yellow-200  animate-duration-1000 animate-delay-400 animate-ping animate-loop">
+    </div>
+    <div class="absolute w-30 h-30 flex justify-center items-center bg-yellow-200  animate-duration-1000 animate-delay-500 animate-ping animate-loop">
+    </div>
+    <div class="w-50 h-50 flex justify-center items-center bg-white shadow-lg z-50">
+      <h1 class="text-xl font-semibold uppercase select-none">
+        BUILDER
+      </h1>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
---
+++ pages/builder.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="w-full h-full flex items-center justify-center ">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
