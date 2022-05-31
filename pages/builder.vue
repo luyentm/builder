0311d661702e6ee1d1865f894fb46005793ef389
Luyen Tran <tranminhluyen.1993@gmail.com>
2022-05-31 17:16:45 +0700

    update

--- pages/builder.vue
+++ pages/builder.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="w-full h-full flex">
-    <div class="w-1/5 border-r border-gray-600"></div>
-    <div class="w-4/5"></div>
+    <div class="w-1/5 border-r border-black"></div>
+    <div class="w-3/5"></div>
+    <div class="w-1/5 border-l border-black"></div>
   </div>
 </template>
 
