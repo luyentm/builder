959a978eb9ba933fae2585b0405a0bc4a7aac541
Luyen Tran <tranminhluyen.1993@gmail.com>
2022-09-08 14:16:04 +0700

    update

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,23 +2,19 @@
   <div class="w-full h-screen flex flex-col bg-gray-800">
     <nuxt class="w-full flex-1" />
     <div
-      class="w-full flex justify-center items-center space-x-3 border-t border-black"
-    >
+      class="w-full flex justify-center items-center space-x-3 border-t border-black">
       <nuxt-link
         class="!outline-none text-gray-400 hover:text-orange-300"
-        to="/"
-        >home</nuxt-link
-      >
+        to="/">home</nuxt-link>
       <nuxt-link
         class="!outline-none text-gray-400 hover:text-orange-300"
-        to="/builder"
-        >builder</nuxt-link
-      >
+        to="/builder">builder</nuxt-link>
       <nuxt-link
         class="!outline-none text-gray-400 hover:text-orange-300"
-        to="/animate"
-        >animate</nuxt-link
-      >
+        to="/animate">animate</nuxt-link>
+      <nuxt-link
+        class="!outline-none text-gray-400 hover:text-orange-300"
+        to="/animation">animation</nuxt-link>
     </div>
   </div>
 </template>
---
+++ pages/expand.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="w-full h-full flex flex-col mt-50 ">
+    <button @click="show = !show">Toggle</button>
+    <div class="w-50 overflow-hidden">
+      <Transition name="slide-fade">
+        <p class="text-yellow-500" v-if="show">hello</p>
+      </Transition>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  data() {
+    return {
+      show: false
+    }
+  }
+}
+</script>
+<style>
+.slide-fade-enter-active {
+  transition: all 0.3s ease-out;
+}
+
+.slide-fade-leave-active {
+  transition: all 0.3s ease-in;
+}
+
+.slide-fade-enter,
+.slide-fade-leave-to {
+  transform: translateX(20px);
+  opacity: 0;
+}
+</style>
